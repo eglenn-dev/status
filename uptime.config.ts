e4685c5b5197db51b15c26d5890f2ceafdd0dd76
Ethan Glenn <ethan+git@eglenn.dev>
2025-08-18 18:12:29 -0600

    Added Resumly back

--- uptime.config.ts
+++ uptime.config.ts
@@ -12,8 +12,7 @@ const pageConfig: PageConfig = {
   // If not specified, all monitors will be shown in a single list
   // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
   group: {
-    '🌐 Web Apps': ['marknote-one', 'clipit-one'],
-    // '🌐 Web Apps': ['resumly-frontend', 'marknote-one', 'clipit-one'],
+    '🌐 Web Apps': ['resumly-frontend', 'marknote-one', 'clipit-one'],
   },
 }
 
@@ -45,17 +44,17 @@ const workerConfig: WorkerConfig = {
     //   // [OPTIONAL] if specified, the response ust contains the keyword to be considered as operational.
     //   responseKeyword: 'Online!',
     // },
-    // {
-    //   id: 'resumly-frontend',
-    //   name: 'Resumly.pro',
-    //   method: 'GET',
-    //   target: 'https://resumly.pro/online',
-    //   statusPageLink: 'https://resumly.pro',
-    //   hideLatencyChart: true,
-    //   expectedCodes: [200],
-    //   timeout: 10000,
-    //   responseKeyword: 'Online!',
-    // },
+    {
+      id: 'resumly-frontend',
+      name: 'Resumly.pro',
+      method: 'GET',
+      target: 'https://resumly.pro/online',
+      statusPageLink: 'https://resumly.pro',
+      hideLatencyChart: true,
+      expectedCodes: [200],
+      timeout: 10000,
+      responseKeyword: 'Online!',
+    },
     {
       id: 'marknote-one',
       name: 'MarkNote.one',
